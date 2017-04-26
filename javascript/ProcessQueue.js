7b96ae578285cda6cb4a3479e736eb57a55c071a
Anselm Christophersen <ac@anselm.dk>
2017-04-26 22:52:04 +0200

    stop processing queue when modal is closed

--- javascript/ProcessQueue.js
+++ javascript/ProcessQueue.js
@@ -23,11 +23,17 @@
               ]
         });
         var opened = false;
+        var closed = false;
+        popup.on( "dialogclose", function( event, ui ) {
+            opened = false;
+            closed = true;
+        } );
 
         var popup_button_selector = '#action_doProcessQueue';
         $(popup_button_selector).entwine({
 
             onclick: function(e){
+                closed = false;
                 var $this = this;
                 $this.addClass('loading');
                 var url = this.data('url');
@@ -41,14 +47,16 @@
                             html = '<h3>Processing. Remaining to be processed:</h3><h2>' + data.Remaining + '/' + data.Total + '</h2><em>Don\'t close this modal before processing is done!</em>';
                         }
                         dialog.html(html);
-                        if (!opened) {
+                        if (!opened && !closed) {
                             dialog.dialog( "open" );
                             opened = true;
                         }
-                        if (data.Status == 'Sending') {
-                            poll();
-                        } else if (data.Status == 'Sent') {
-                            location.reload();
+                        if (!closed) {
+                            if (data.Status == 'Sending') {
+                                poll();
+                            } else if (data.Status == 'Sent') {
+                                location.reload();
+                            }
                         }
                     }, 'json')
                 };
