c9da8d68ceff95254f781f9a729feaddbe39bba3
jochenguelden <jochen@guelden.org>
2019-09-25 13:01:27 +0200

    fixing subscriptiom

--- code/extensions/MemberValidator.php
+++ code/extensions/MemberValidator.php
@@ -15,22 +15,30 @@ class MemberValidator extends RequiredFields {
             $this->validationError('Email','This is not a valid Email','required');
             return;
         }
-
+        
+        
         // Check if an Email exists but belongs to a different User
         // If so we dont want to add this user and so avoid duplicates
         $ExistingRecipient = Member::get()->filter('Email', $data['Email'])->First();
-
-        if (isset($ExistingRecipient)) {
+        
+        
+        if ($ExistingRecipient->ID > 0) {
 
             if($ExistingRecipient->Email == $data['Email']) {
+              
+              
                 // Es gibt schon einen User mit dieser Email
 
                 $MailingListID = $data['MailingListID'];
-
+                $Recipient = new RecipientExtension();
                 // Is already subscribed. maybe just add the user to another list?
-                if (Member::inMailingList( $ExistingRecipient, $MailingListID )) {
+                if ( $Recipient::inMailingList( $ExistingRecipient, $MailingListID )) {
                     // TODO write correct error message and show message in form
                     $this->validationError('Email','You have already subscribed to this mailing list','required');
+                    $bRet = false;
+                    return $bRet;
+                } else {
+                  
                 }
 
             } else {
