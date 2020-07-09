ca75cb2c4219154f1cdae5c186dc119a58c5724a
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-07-09 08:07:35 +0530

    fixes

--- code/Model/MailingList.php
+++ code/Model/MailingList.php
@@ -93,9 +93,12 @@ class MailingList extends DataObject {
         // $FilterableFields->dataFieldByName('Filter_Member_BadgeType')->setTitle('Ausweisart');
 
 
-        $FilterableFields->dataFieldByName('Filter_Member_WillAssist')
-            ->setTitle('Teilnahme bestätigt')
-            ->setDescription('Keine Auswahl für Alle');
+        if($FilterableFields->dataFieldByName('Filter_Member_WillAssist')) {
+            $FilterableFields->dataFieldByName('Filter_Member_WillAssist')
+                ->setTitle('Teilnahme bestätigt')
+                ->setDescription('Keine Auswahl für Alle');
+        }
+
 
         // $FilterableFields->dataFieldByName('Filter_Member_NDAAccepted')->setTitle('Geheimhaltungserklärung akzeptiert');
         // // $FilterableFields->dataFieldByName('Filter_Member_CountryImport')->setTitle('CountryImport');
